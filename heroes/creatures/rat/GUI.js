3fc0bdf07945ee82742c00b398626162a6d5e1f0
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:25:22 -0400

    Create GUI.js

---
+++ heroes/creatures/rat/GUI.js
@@ -0,0 +1,10 @@
+window.RatData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Rat',
+  icon: '🐀',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
