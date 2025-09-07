73683f0c8338645e23cd918b406c086e9ae2b0c4
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:49:27 -0400

    Create GUI.js

---
+++ heroes/humans/warrior/GUI.js
@@ -0,0 +1,10 @@
+window.WarriorData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Warrior',
+  icon: '⚔🗡️',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
