e37aa480dd021ec3907b9b2a06514f3e1518f519
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:02:27 -0400

    Create GUI.js

---
+++ heroes/legends/dinosaur/GUI.js
@@ -0,0 +1,13 @@
+window.DinosaurData = {
+  category: 'Legends',
+  id: 'legends',
+  name: 'Dinosaur',
+  icon: '🥚',
+  abilities: [
+    { choice: 'ability1', label: 'Tyrannosaurus', description: 'place-holder' },
+    { choice: 'ability2', label: 'Spinosaurus', description: 'place-holder' },
+    { choice: 'ability3', label: 'Pterodactyl', description: 'place-holder' },
+    { choice: 'ability4', label: 'Velociraptor', description: 'place-holder' },
+    { choice: 'ability5', label: 'Dilophosaurus', description: 'place-holder' }
+  ]
+};
