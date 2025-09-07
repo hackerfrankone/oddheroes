224d13eaad22dcf5d5245d99105d9234398e8f13
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:34:27 -0400

    Create GUI.js

---
+++ heroes/legends/phoenix/GUI.js
@@ -0,0 +1,10 @@
+window.PhoenixData = {
+  category: 'Legends',
+  id: 'legends',
+  name: 'Phoenix',
+  icon: '🔥',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
