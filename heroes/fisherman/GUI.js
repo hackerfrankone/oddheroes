c8cc6062cc5cda7bed252506fe68eec3184aeebb
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:53:17 -0400

    Create GUI.js

---
+++ heroes/fisherman/GUI.js
@@ -0,0 +1,10 @@
+window.FishermanData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Fisherman',
+  icon: '🎣',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
