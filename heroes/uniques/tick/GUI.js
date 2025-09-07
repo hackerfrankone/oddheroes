208402817a122cc3bf46a25c2c0858a5cb7a710d
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:51:34 -0400

    Create GUI.js

---
+++ heroes/uniques/tick/GUI.js
@@ -0,0 +1,10 @@
+window.TickData = {
+  category: 'Uniques',
+  id: 'uniques',
+  name: 'Tick',
+  icon: '🕷️',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
