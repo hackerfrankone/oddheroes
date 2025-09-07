d62f28c1e550990c01ee94898fc014726e25db59
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 16:47:59 -0400

    Update GUI.js

--- heroes/uniques/tank/GUI.js
+++ heroes/uniques/tank/GUI.js
@@ -2,7 +2,7 @@ window.TankData = {
   category: 'Uniques', 
   id: 'uniques',
   name: 'Tank',
-  icon: '🚜',
+  icon: '🪖',
   abilities: [
     { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
     { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
