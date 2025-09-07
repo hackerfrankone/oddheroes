f15d4612716897d8558c8dd5ce4615760398067c
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:50:27 -0400

    Create abilities.js

---
+++ heroes/humans/miner/abilities.js
@@ -0,0 +1,34 @@
+window.MinerAbilities = {
+  name: 'Miner',
+  abilities: {
+    ability1: {
+      label: 'place-holder',
+      damage: 10,
+      execute: function(gameState, target) {
+        //place-holder
+        console.log('Miner uses ability1 on', target);
+        target.health = (target.health || 100) - this.damage;
+        console.log(`${target.name} health reduced to ${target.health}`);
+        return {
+          success: true,
+          message: `Miner's ability1 dealt ${this.damage} damage to ${target.name}!`
+        };
+      }
+    },
+    ability2: {
+      label: 'place-holder',
+      damage: 0,
+      execute: function(gameState, self) {
+        //place-holder
+        console.log('Miner uses ability2');
+        //place-holder
+        return {
+          success: true,
+          message: `Miner's ability2 activated!`
+        };
+      }
+    }
+  }
+};
+window.abilityMap = window.abilityMap || {};
+window.abilityMap['Miner'] = window.MinerAbilities;
