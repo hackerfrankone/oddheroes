d84dd27e698e5016486d7afbf64d25949b7c971c
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 17:58:13 -0400

    Create GUI.js

---
+++ heroes/uniques/pilot/GUI.js
@@ -0,0 +1,11 @@
+window.PilotData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Pilot',
+  icon: '🪖',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability3', label: 'place-holder', description: 'place-holder' }
+  ]
+};
