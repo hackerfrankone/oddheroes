8a6cbb296bbe2ddee275918aecda8d5b292f92bb
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:57:23 -0400

    Update GUI.js

--- heroes/uniques/leech/GUI.js
+++ heroes/uniques/leech/GUI.js
@@ -2,7 +2,7 @@ window.LeechData = {
   category: 'Uniques',
   id: 'uniques',
   name: 'Leech',
-  icon: '🪰',
+  icon: '🐛',
   abilities: [
     { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
     { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
