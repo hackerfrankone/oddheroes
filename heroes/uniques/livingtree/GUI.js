4dc9833da36fc598675fc204ac85fd06642dae15
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:56:37 -0400

    Create GUI.js

---
+++ heroes/uniques/livingtree/GUI.js
@@ -0,0 +1,10 @@
+window.livingtreeData = {
+  category: 'Uniques',
+  id: 'uniques',
+  name: 'LivingTree',
+  icon: '🌱',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
