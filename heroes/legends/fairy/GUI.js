d26afa1253d1d78706dae23b6cb12b8434be97f7
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 13:54:05 -0400

    Create GUI.js

---
+++ heroes/legends/fairy/GUI.js
@@ -0,0 +1,10 @@
+window.FairyData = {
+  category: 'Legends',
+  id: 'legends',
+  name: 'Fairy',
+  icon: '🧚‍♀️',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
