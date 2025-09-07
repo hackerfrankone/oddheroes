581ffb4ad7a397ca5ff5e19e814e772ead6c0f52
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:11:15 -0400

    Create GUI.js

---
+++ heroes/humans/hunter/GUI.js
@@ -0,0 +1,10 @@
+window.HunterData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Hunter',
+  icon: '🏹',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
