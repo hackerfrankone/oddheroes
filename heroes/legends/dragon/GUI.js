bfa5a1f8aff13977b825c763c9cf0073ba22d9ae
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:05:23 -0400

    Create GUI.js

---
+++ heroes/legends/dragon/GUI.js
@@ -0,0 +1,10 @@
+window.DragonData = {
+  category: 'Legends',
+  id: 'legends',
+  name: 'Dragon',
+  icon: '🐉',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
