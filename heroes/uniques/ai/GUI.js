79e751e5a196210ea5044f6039946b2a21d014d8
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 16:49:57 -0400

    Create GUI.js

---
+++ heroes/uniques/ai/GUI.js
@@ -0,0 +1,10 @@
+window.AIData = {
+  category: 'Uniques',
+  id: 'uniques',
+  name: 'AI',
+  icon: '🤖',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
