d39a764728d4a09178d509e8cf1dda2272ecbff2
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 13:14:08 -0400

    Create GUI.js

---
+++ heroes/creatures/frog/GUI.js
@@ -0,0 +1,10 @@
+window.FrogData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Frog',
+  icon: '🐸',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
