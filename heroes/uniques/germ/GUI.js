d4b464cb0fbeee59c7c63c79d307b54b24551e98
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:53:22 -0400

    Create GUI.js

---
+++ heroes/uniques/germ/GUI.js
@@ -0,0 +1,10 @@
+window.GermData = {
+  category: 'Uniques',
+  id: 'uniques',
+  name: 'Germ',
+  icon: '🦠',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
