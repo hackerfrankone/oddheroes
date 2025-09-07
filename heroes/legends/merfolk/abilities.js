459411185b25378c20fae0a0efd21e053c8aaf6b
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:33:44 -0400

    Create abilities.js

---
+++ heroes/legends/merfolk/abilities.js
@@ -0,0 +1,34 @@
+window.MerfolkAbilities = {
+  name: 'Merfolk',
+  abilities: {
+    ability1: {
+      label: 'place-holder',
+      damage: 12,
+      execute: function(gameState, target) {
+        //place-holder
+        console.log('Merfolk uses ability1 on', target);
+        target.health = (target.health || 100) - this.damage;
+        console.log(`${target.name} health reduced to ${target.health}`);
+        return {
+          success: true,
+          message: `Merfolk's ability1 dealt ${this.damage} damage to ${target.name}!`
+        };
+      }
+    },
+    ability2: {
+      label: 'place-holder',
+      damage: 0,
+      execute: function(gameState, self) {
+        //place-holder
+        console.log('Merfolk uses ability2');
+        //place-holder
+        return {
+          success: true,
+          message: `Merfolk's ability2 activated!`
+        };
+      }
+    }
+  }
+};
+window.abilityMap = window.abilityMap || {};
+window.abilityMap['Merfolk'] = window.MerfolkAbilities;
