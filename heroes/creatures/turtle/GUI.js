94ed868cc31e3c6059e7d87b7e3eaac811c9b91f
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:17:21 -0400

    Create GUI.js

---
+++ heroes/creatures/turtle/GUI.js
@@ -0,0 +1,10 @@
+window.TurtleData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Turtle',
+  icon: '🐢',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
