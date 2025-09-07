3f82e61d629de07e1b60a2868895b32065a7d223
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 12:47:40 -0400

    Update GUI.js

--- heroes/frog/GUI.js
+++ heroes/frog/GUI.js
@@ -1,1 +1,10 @@
-// place-holder
+window.FrogData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Frog',
+  icon: '🐸',
+  abilities: [
+    { choice: 'ability1', label: 'Tongue Lash', description: 'Deals 10 damage to a target' },
+    { choice: 'ability2', label: 'Jump Boost', description: 'Increases speed by 2 for 2 turns' }
+  ]
+};
