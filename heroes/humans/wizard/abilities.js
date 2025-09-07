ffc846fadfc62c9a3b27644f489d4c552352be89
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:09:11 -0400

    Create abilities.js

---
+++ heroes/humans/wizard/abilities.js
@@ -0,0 +1,34 @@
+window.WizardAbilities = {
+  name: 'Wizard',
+  abilities: {
+    ability1: {
+      label: 'place-holder',
+      damage: 12,
+      execute: function(gameState, target) {
+        //place-holder
+        console.log('Wizard uses ability1 on', target);
+        target.health = (target.health || 100) - this.damage;
+        console.log(`${target.name} health reduced to ${target.health}`);
+        return {
+          success: true,
+          message: `Wizard's ability1 dealt ${this.damage} damage to ${target.name}!`
+        };
+      }
+    },
+    ability2: {
+      label: 'place-holder',
+      damage: 0,
+      execute: function(gameState, self) {
+        //place-holder
+        console.log('Wizard uses ability2');
+        //place-holder
+        return {
+          success: true,
+          message: `Wizard's ability2 activated!`
+        };
+      }
+    }
+  }
+};
+window.abilityMap = window.abilityMap || {};
+window.abilityMap['Wizard'] = window.WizardAbilities;
