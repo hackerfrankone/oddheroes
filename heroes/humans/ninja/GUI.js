f6294f5afc33617b6235c5ee084b9f7b259e5817
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:57:29 -0400

    Create GUI.js

---
+++ heroes/humans/ninja/GUI.js
@@ -0,0 +1,10 @@
+window.NinjaData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Ninja',
+  icon: '🥷',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
