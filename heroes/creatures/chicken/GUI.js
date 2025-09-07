bb88b99329cc8ba5462bb912282fda4fac13ddb0
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:09:41 -0400

    Create GUI.js

---
+++ heroes/creatures/chicken/GUI.js
@@ -0,0 +1,10 @@
+window.ChickenData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Chicken',
+  icon: '🐔',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
