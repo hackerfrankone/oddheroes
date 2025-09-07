0cec745010067d1ec85b9a39446daa078e72d04b
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:47:30 -0400

    Create GUI.js

---
+++ heroes/humans/alchemist/GUI.js
@@ -0,0 +1,10 @@
+window.AlchemistData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Alchemist',
+  icon: '🧪',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
