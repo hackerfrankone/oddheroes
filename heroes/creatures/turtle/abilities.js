3d9e837f0f535180ef1ec8739ac8b9dd39cfb22f
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:17:49 -0400

    Create abilities.js

---
+++ heroes/creatures/turtle/abilities.js
@@ -0,0 +1,34 @@
+window.TurtleAbilities = {
+  name: 'Turtle',
+  abilities: {
+    ability1: {
+      label: 'place-holder',
+      damage: 8,
+      execute: function(gameState, target) {
+        //place-holder
+        console.log('Turtle uses ability1 on', target);
+        target.health = (target.health || 100) - this.damage;
+        console.log(`${target.name} health reduced to ${target.health}`);
+        return {
+          success: true,
+          message: `Turtle's ability1 dealt ${this.damage} damage to ${target.name}!`
+        };
+      }
+    },
+    ability2: {
+      label: 'place-holder',
+      damage: 0,
+      execute: function(gameState, self) {
+        //place-holder
+        console.log('Turtle uses ability2');
+        //place-holder
+        return {
+          success: true,
+          message: `Turtle's ability2 activated!`
+        };
+      }
+    }
+  }
+};
+window.abilityMap = window.abilityMap || {};
+window.abilityMap['Turtle'] = window.TurtleAbilities;
