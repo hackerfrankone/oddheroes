6860b6c2047373706b27ecef7e9bb23d0ee65aed
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:49:47 -0400

    Create GUI.js

---
+++ heroes/uniques/mosquito/GUI.js
@@ -0,0 +1,10 @@
+window.MosquitoSwarmData = {
+  category: 'Uniques',
+  id: 'uniques',
+  name: 'Mosquito Swarm',
+  icon: '🦟',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
