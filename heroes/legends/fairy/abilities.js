5d7ee10cc09f52d71285a7fd01c0d90ba0af266e
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 13:54:32 -0400

    Create abilities.js

---
+++ heroes/legends/fairy/abilities.js
@@ -0,0 +1,34 @@
+window.FairyAbilities = {
+  name: 'Fairy',
+  abilities: {
+    ability1: {
+      label: 'place-holder',
+      damage: 8,
+      execute: function(gameState, target) {
+        //place-holder
+        console.log('Fairy uses ability1 on', target);
+        target.health = (target.health || 100) - this.damage;
+        console.log(`${target.name} health reduced to ${target.health}`);
+        return {
+          success: true,
+          message: `Fairy's ability1 dealt ${this.damage} damage to ${target.name}!`
+        };
+      }
+    },
+    ability2: {
+      label: 'place-holder',
+      damage: 0,
+      execute: function(gameState, self) {
+        //place-holder
+        console.log('Fairy uses ability2');
+        //place-holder
+        return {
+          success: true,
+          message: `Fairy's ability2 activated!`
+        };
+      }
+    }
+  }
+};
+window.abilityMap = window.abilityMap || {};
+window.abilityMap['Fairy'] = window.FairyAbilities;
