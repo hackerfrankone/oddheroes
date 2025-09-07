c6fd66e9afe3993f791135f8bc2bb48162b95307
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:33:08 -0400

    Create GUI.js

---
+++ heroes/legends/merfolk/GUI.js
@@ -0,0 +1,10 @@
+window.MerfolkData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Merfolk',
+  icon: '🧜‍♂️',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
