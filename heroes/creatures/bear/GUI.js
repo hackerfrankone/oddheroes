a988313b6be77445bc00d0dd1038194836aee711
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:18:46 -0400

    Create GUI.js

---
+++ heroes/creatures/bear/GUI.js
@@ -0,0 +1,10 @@
+window.BearData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Bear',
+  icon: '🐻',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
