1b6f2f0c4336aa8cfebe54c0de61a758291a570c
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:07:21 -0400

    Create GUI.js

---
+++ heroes/legends/ghost/GUI.js
@@ -0,0 +1,10 @@
+window.GhostData = {
+  category: 'Legends',
+  id: 'legends',
+  name: 'Ghost',
+  icon: '👻',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
