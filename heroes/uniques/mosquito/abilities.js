7965d7b9419184ea97e235cafc08b50efd23cde8
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:51:14 -0400

    Create abilities.js

---
+++ heroes/uniques/mosquito/abilities.js
@@ -0,0 +1,34 @@
+window.MosquitoSwarmAbilities = {
+  name: 'Mosquito Swarm',
+  abilities: {
+    ability1: {
+      label: 'place-holder',
+      damage: 7,
+      execute: function(gameState, target) {
+        //place-holder
+        console.log('Mosquito Swarm uses ability1 on', target);
+        target.health = (target.health || 100) - this.damage;
+        console.log(`${target.name} health reduced to ${target.health}`);
+        return {
+          success: true,
+          message: `Mosquito Swarm's ability1 dealt ${this.damage} damage to ${target.name}!`
+        };
+      }
+    },
+    ability2: {
+      label: 'place-holder',
+      damage: 0,
+      execute: function(gameState, self) {
+        //place-holder
+        console.log('Mosquito Swarm uses ability2');
+        //place-holder
+        return {
+          success: true,
+          message: `Mosquito Swarm's ability2 activated!`
+        };
+      }
+    }
+  }
+};
+window.abilityMap = window.abilityMap || {};
+window.abilityMap['Mosquito Swarm'] = window.MosquitoSwarmAbilities;
