bde6af82ee4a3df8d8b50966486e4a36f3ec73ee
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:52:38 -0400

    Create abilities.js

---
+++ heroes/hunter/abilities.js
@@ -0,0 +1,34 @@
+window.HunterAbilities = {
+  name: 'Hunter',
+  abilities: {
+    ability1: {
+      label: 'place-holder',
+      damage: 14,
+      execute: function(gameState, target) {
+        //place-holder
+        console.log('Hunter usesmeier1 on', target);
+        target.health = (target.health || 100) - this.damage;
+        console.log(`${target.name} health reduced to ${target.health}`);
+        return {
+          success: true,
+          message: `Hunter's ability1 dealt ${this.damage} damage to ${target.name}!`
+        };
+      }
+    },
+    ability2: {
+      label: 'place-holder',
+      damage: 0,
+      execute: function(gameState, self) {
+        //place-holder
+        console.log('Hunter uses ability2');
+        //place-holder
+        return {
+          success: true,
+          message: `Hunter's ability2 activated!`
+        };
+      }
+    }
+  }
+};
+window.abilityMap = window.abilityMap || {};
+window.abilityMap['Hunter'] = window.HunterAbilities;
