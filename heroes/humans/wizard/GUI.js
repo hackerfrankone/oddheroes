5cc5feeeef1781eddf8aaea6ab22f7746956ed59
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:08:42 -0400

    Create GUI.js

---
+++ heroes/humans/wizard/GUI.js
@@ -0,0 +1,10 @@
+window.WizardData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Wizard',
+  icon: '🧙‍♂️',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
