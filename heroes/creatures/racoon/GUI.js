2e658510641491cb09f76e0f13927b14d0c54284
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:20:18 -0400

    Create GUI.js

---
+++ heroes/creatures/racoon/GUI.js
@@ -0,0 +1,10 @@
+window.RaccoonData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Raccoon',
+  icon: '🦝',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
