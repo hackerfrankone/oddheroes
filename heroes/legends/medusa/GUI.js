b42601a62b665bdc670fde89bf4aa8cfcc5d2e3a
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 13:55:46 -0400

    Create GUI.js

---
+++ heroes/legends/medusa/GUI.js
@@ -0,0 +1,10 @@
+window.MedusaData = {
+  category: 'Legends',
+  id: 'legends',
+  name: 'Medusa',
+  icon: '🐍',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
