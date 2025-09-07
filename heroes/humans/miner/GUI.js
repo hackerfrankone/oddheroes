28017068e89d0c00024a3318d83f8cb5e835bbdc
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:50:08 -0400

    Create GUI.js

---
+++ heroes/humans/miner/GUI.js
@@ -0,0 +1,10 @@
+window.MinerData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Miner',
+  icon: '⛏️',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
