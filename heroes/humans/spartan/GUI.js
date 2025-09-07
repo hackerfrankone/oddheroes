85164942d852921ee99a3706a6c1c6efbe2ca9f6
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 15:22:29 -0400

    Create GUI.js

---
+++ heroes/humans/spartan/GUI.js
@@ -0,0 +1,10 @@
+window.SpartanData = {
+  category: 'Humans',
+  id: 'humans',
+  name: 'Spartan',
+  icon: '🛡️',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
