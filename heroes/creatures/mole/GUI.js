83d740549daffc99cca79a553989991de11a1f9d
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:21:57 -0400

    Create GUI.js

---
+++ heroes/creatures/mole/GUI.js
@@ -0,0 +1,10 @@
+window.MoleData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Mole',
+  icon: '🐹',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
