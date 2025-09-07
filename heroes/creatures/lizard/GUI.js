2004e0addc0ffa262af1892a7f01b132863f34ee
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:21:02 -0400

    Create GUI.js

---
+++ heroes/creatures/lizard/GUI.js
@@ -0,0 +1,10 @@
+window.LizardData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Lizard',
+  icon: '🦎',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
