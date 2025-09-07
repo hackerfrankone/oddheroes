cf0dd8cc71545848e3ad11958cc1623240dd31ec
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 13:32:24 -0400

    Create big-footGUI.js

---
+++ heroes/legends/big-foot/big-footGUI.js
@@ -0,0 +1,10 @@
+window.BigFootData = {
+  category: 'Legends',
+  id: 'legends',
+  name: 'Big-Foot',
+  icon: '🦶',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
