c36508de8243cfbc8302fd34f2e74fac363ffc8c
hackerfrankone <hackerfrankone@gmail.com>
2025-09-07 14:27:11 -0400

    Create GUI.js

---
+++ heroes/creatures/mongoose/GUI.js
@@ -0,0 +1,10 @@
+window.MongooseData = {
+  category: 'Creatures',
+  id: 'creatures',
+  name: 'Mongoose',
+  icon: '🦡',
+  abilities: [
+    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+  ]
+};
