ca6c6fb83df938180282190f23424d8c15adb9a6
hackerfrankone <hackerfrankone@gmail.com>
2025-09-08 13:25:00 -0400

    Update GUI.js

--- heroes/creatures/frog/GUI.js
+++ heroes/creatures/frog/GUI.js
@@ -5,7 +5,7 @@ const FrogData = {
   id: 'creatures',
   abilities: [
     { choice: 'Duplication', label: 'Duplication', description: 'Lay 3 frog eggs. 5 turn CD. Turn 4 will complete the evolution process. If eggs near water, tadpoles will emerge for scouts.' },
-    { choice: 'Evolution', label: 'Evolution', description: 'Evolution into a giant BullFrog. Stats are tripled.' },
+    { choice: 'Evolution', label: 'Evolution', description: 'Evolution into a giant Goliath BullFrog. Stats are tripled.' },
     { choice: 'Poison', label: 'Poison Tongue', description: 'Tongue lash no longer does dmg but will cause target to be deadly poisoned. -10hp per turn.' }
   ],
   hp: 20,
