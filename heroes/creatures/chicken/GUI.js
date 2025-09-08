87ae6b6c5d2c691f1c93649e5edee411ff384a98
hackerfrankone <hackerfrankone@gmail.com>
2025-09-08 15:21:50 -0400

    Update GUI.js

--- heroes/creatures/chicken/GUI.js
+++ heroes/creatures/chicken/GUI.js
@@ -1,10 +1,29 @@
-window.ChickenData = {
-  category: 'Creatures',
-  id: 'creatures',
+const ChickenData = {
   name: 'Chicken',
   icon: '🐔',
+  category: 'Creatures',
+  id: 'creatures',
   abilities: [
-    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Chicken', label: 'Chicken', description: 'Choose friendly hero to eat this incredible healthy tasty chicken to recover +40 HP. This hero dies. Must be within tiles.' },
+    { choice: 'Flap', label: 'Flap', description: 'Flaps wings to create a gust, pushing enemies back 2 tiles.' },
+    { choice: 'EggDrop', label: 'Egg Drop', 'Egg drop onto a tile, Any hero may eat egg to recover 10 HP. 1 turn CD.' }
+  ],
+  hp: 25,
+  speed: 15,
+  tileMove: 2,
+  passive: {
+    label: 'Search.',
+    description: 'End of every turn, this hero will search under-ground for food if successful +5 HP'
+  },
+  moveset: [
+    {
+      label: 'Move 1: Peck deals 10 HP.',
+      description: 'A placeholder move for Chicken in the battle arena.'
+    },
+    {
+      label: 'Move 2: Roost onto a tree to recover 20 HP',
+      description: 'Another placeholder move for Chicken in the battle arena.'
+    }
   ]
 };
+window.ChickenData = ChickenData;
