4d2aa764bb761fefe08a254b6276e03892fb69e0
hackerfrankone <hackerfrankone@gmail.com>
2025-09-08 14:39:03 -0400

    Update GUI.js

--- heroes/creatures/bear/GUI.js
+++ heroes/creatures/bear/GUI.js
@@ -4,24 +4,24 @@ const BearData = {
   category: 'Creatures',
   id: 'creatures',
   abilities: [
-    { choice: 'n/a', label: 'Claw Swipe', description: 'n/a.' },
-    { choice: 'n/a', label: 'Roar', description: 'n/a.' },
-    { choice: 'n/a', label: 'Hibernate', description: 'n/a.' }
+    { choice: 'Rage', label: 'Rage', description: 'n/a.' },
+    { choice: 'Roar of Terror', label: 'Roar of Terror', description: 'All surrounding tilesx3 from hero cant move for 2 turns. ' },
+    { choice: 'Hibernate', label: 'Hibernate', description: 'Sleep for 3 turns to recover 25 HP' }
   ],
   hp: 40,
   speed: 15,
   tileMove: 2,
   passive: {
-    label: 'N/A',
-    description: ''
+    label: 'Smell',
+    description: 'Can see camoflauged heroes if within field of view/fog of war'
   },
   moveset: [
     {
-      label: 'Move 1: Bite & Claw deals 20 HP',
+      label: 'Move 1: Bite & Swipe deals 20 HP',
       description: 'A placeholder move for Bear in the battle arena.'
     },
     {
-      label: 'Move 2: Placeholder',
+      label: 'Move 2: n/a',
       description: 'Another placeholder move for Bear in the battle arena.'
     }
   ]
