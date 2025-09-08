c62f8ddd7fae379ac1d74aab77ce50f76249d461
hackerfrankone <hackerfrankone@gmail.com>
2025-09-08 16:50:47 -0400

    Update GUI.js

--- heroes/creatures/chicken/GUI.js
+++ heroes/creatures/chicken/GUI.js
@@ -6,7 +6,7 @@ const ChickenData = {
   abilities: [
     { choice: 'Chicken', label: 'Chicken', description: 'Choose friendly hero to eat this incredible healthy tasty chicken to recover +40 HP. This hero dies. Must be within tiles.' },
     { choice: 'Flap', label: 'Flap', description: 'Flaps wings to create a gust, pushing enemies back 2 tiles.' },
-    { choice: 'EggDrop', label: 'Egg Drop', 'Egg drop onto a tile, Any hero may eat egg to recover 10 HP. 1 turn CD.' }
+    { choice: 'EggDrop', label: 'Egg Drop', description: 'Egg drop onto a tile, Any hero may eat egg to recover 10 HP. 1 turn CD.' }
   ],
   hp: 25,
   speed: 15,
