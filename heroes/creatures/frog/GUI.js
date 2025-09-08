32b78297ed6b97a3ed5319757c73b74d360482a6
hackerfrankone <hackerfrankone@gmail.com>
2025-09-08 10:52:09 -0400

    Update GUI.js

--- heroes/creatures/frog/GUI.js
+++ heroes/creatures/frog/GUI.js
@@ -4,16 +4,16 @@ const FrogData = {
   category: 'Creatures',
   id: 'creatures',
   abilities: [
-    { choice: 'Duplication', label: 'Duplication', description: 'place-holder' },
-    { choice: 'Evolution', label: 'Evolution', description: 'place-holder' },
-    { choice: 'Poison', label: 'Poison', description: 'place-holder' }
+    { choice: 'Duplication', label: 'Duplication', description: 'Lay 3 frog eggs. 5 turn CD. Turn 4 will complete the evolution process. If eggs near water, tadpoles will emerge for scouts.' },
+    { choice: 'Evolution', label: 'Evolution', description: 'Evolution into BullFrog with boosted stats. +20HP, +10dmg, +2 tile move, +2 regeneration.' },
+    { choice: 'Poison', label: 'Poison Tongue', description: 'Tongue lash will cause target to be deadly poisoned. -5hp per turn.' }
   ],
-  hp: 30,
-  speed: 30,
-  tileMove: 3,
+  hp: 20,
+  speed: 20,
+  tileMove: 2,
   passive: {
-    label: 'Jumplickity',
-    description: 'Read above lol.'
+    label: 'Regenerate',
+    description: '+2 HP in or near water.'
   }
 };
 window.FrogData = FrogData;
