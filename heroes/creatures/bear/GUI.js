786fc8261742374ff4db5ede0f7186eba4e5c840
hackerfrankone <hackerfrankone@gmail.com>
2025-09-08 13:26:57 -0400

    Update GUI.js

--- heroes/creatures/bear/GUI.js
+++ heroes/creatures/bear/GUI.js
@@ -1,10 +1,19 @@
-window.BearData = {
-  category: 'Creatures',
-  id: 'creatures',
+const BearData = {
   name: 'Bear',
   icon: '🐻',
+  category: 'Creatures',
+  id: 'creatures',
   abilities: [
-    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
-  ]
+    { choice: 'ClawSwipe', label: 'Claw Swipe', description: 'Swipes with powerful claws, dealing damage to a single target.' },
+    { choice: 'Roar', label: 'Roar', description: 'Intimidates enemies, reducing their attack strength temporarily.' },
+    { choice: 'Hibernate', label: 'Hibernate', description: 'Enters a defensive state, recovering HP over time.' }
+  ],
+  hp: 40,
+  speed: 15,
+  tileMove: 2,
+  passive: {
+    label: 'N/A',
+    description: ''
+  }
 };
+window.BearData = BearData;
