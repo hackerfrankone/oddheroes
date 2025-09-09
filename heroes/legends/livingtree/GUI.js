8ab0165c23c8c53e76a25e8d6c3760447a49ae56
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 17:00:17 -0400

    Update GUI.js

--- heroes/legends/livingtree/GUI.js
+++ heroes/legends/livingtree/GUI.js
@@ -2,7 +2,7 @@ const LivingTreeData = {
   name: 'LivingTree',
   icon: '🌳',
   category: 'Legends',
-  id: 'Legends',
+  id: 'legends',
   abilities: [
     { choice: 'Tree Walker', label: 'Tree Walker', description: 'Able to move and provide camouflage looks like an in-game tree.' },
     { choice: 'Tree Elder', label: 'Tree Elder', description: 'Spawn a tumble weed with 1 HP, deals 1 HP and moves 5 tiles. 4 turn CD. Spawn a flytrap near LivingTree with 1 HP, deals 3 HP, cant move. 4 turn CD.' }
