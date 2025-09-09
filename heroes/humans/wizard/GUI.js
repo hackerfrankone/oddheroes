4ec6cd49f8f6a059dfd6f502b1644261f59e00e9
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 14:06:16 -0400

    Update GUI.js

--- heroes/humans/wizard/GUI.js
+++ heroes/humans/wizard/GUI.js
@@ -4,23 +4,23 @@ const WizardData = {
   category: 'Humans',
   id: 'humans',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Ultimate spell', label: 'Ultimate', description: 'Deal 40 HP AOE, including underground. Uses 10 mana.' },
+    { choice: 'Illusions', label: 'place-holder', description: 'Create 2 Illusions of this hero. They cant cast spells. You can Switch positions with any illusions on map. Uses 10 mana. Switching positions uses 6 mana.' }
   ],
-  hp: 11,
+  hp: 21,
   speed: 30,
   tileMove: 3,
   passive: {
-    label: 'N/A',
-    description: ''
+    label: '10 Mana',
+    description: 'Spells cost mana. Each turn, you passively regenerate 2 mana. Begin the game with 2 mana. '
   },
   moveset: [
     {
-      label: 'Move 1: Placeholder',
+      label: 'Move 1: Fire Ball deals 25 HP. 5 tile rage. Uses 4 mana.',
       description: 'A placeholder move for Wizard in the battle arena.'
     },
     {
-      label: 'Move 2: Placeholder',
+      label: 'Move 2: Recover 20 HP. Uses 6 mana. Can cast on any friendly hero within 2 tiles.',
       description: 'Another placeholder move for Wizard in the battle arena.'
     }
   ]
