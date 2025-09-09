daeec02201346ee3e0f3450df0d2c0939166976a
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 16:47:24 -0400

    Create abilities.js

---
+++ heroes/legends/livingtree/abilities.js
@@ -0,0 +1,34 @@
+window.LivingTreeAbilities = {
+  name: 'LivingTree',
+  abilities: {
+    ability1: {
+      label: 'place-holder',
+      damage: 10,
+      execute: function(gameState, target) {
+        //place-holder
+        console.log('LivingTree uses ability1 on', target);
+        target.health = (target.health || 100) - this.damage;
+        console.log(`${target.name} health reduced to ${target.health}`);
+        return {
+          success: true,
+          message: `LivingTree's ability1 dealt ${this.damage} damage to ${target.name}!`
+        };
+      }
+    },
+    ability2: {
+      label: 'place-holder',
+      damage: 0,
+      execute: function(gameState, self) {
+        //place-holder
+        console.log('LivingTree uses ability2');
+        //place-holder
+        return {
+          success: true,
+          message: `LivingTree's ability2 activated!`
+        };
+      }
+    }
+  }
+};
+window.abilityMap = window.abilityMap || {};
+window.abilityMap['LivingTree'] = window.LivingTreeAbilities;
