2aef89bfc02986a223ba9e729427ff6455a5432b
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 12:40:43 -0400

    Update GUI.js

--- heroes/humans/hunter/GUI.js
+++ heroes/humans/hunter/GUI.js
@@ -4,23 +4,23 @@ const HunterData = {
   category: 'Humans',
   id: 'humans',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Bow & Arrows', label: 'Bow & Arrows', description: 'This has 5 tile rage and deals 20 HP. },
+    { choice: 'Crossbow & Bolts', label: 'Crossbow & Bolts', description: 'Shoot 5 bolts in one turn to deal 50 HP. Reloading bolts: 4 turn CD.' }
   ],
-  hp: 24,
-  speed: 20,
-  tileMove: 3,
+  hp: 55,
+  speed: 45,
+  tileMove: 4,
   passive: {
-    label: 'N/A',
-    description: ''
+    label: 'Quiver',
+    description: 'holds 5 arrows or 15 bolts.'
   },
   moveset: [
     {
-      label: 'Move 1: Placeholder',
+      label: 'Move 1: Shoot Bow or CrossBow HP depends on ability picked.',
       description: 'A placeholder move for Hunter in the battle arena.'
     },
     {
-      label: 'Move 2: Placeholder',
+      label: 'Move 2: Craft home made Arrows or Bolts. Craft 5 arrows at once or 5 bolts at once. These are half HP from original arrows and bolts.',
       description: 'Another placeholder move for Hunter in the battle arena.'
     }
   ]
