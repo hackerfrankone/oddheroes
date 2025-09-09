4449d3e4e6a4dad08375d6ece20e4a780fa354e9
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 12:04:34 -0400

    Update GUI.js

--- heroes/humans/miner/GUI.js
+++ heroes/humans/miner/GUI.js
@@ -7,9 +7,9 @@ const MinerData = {
     { choice: 'Digger', label: 'Digger', description: 'This hero creates a tunnel underground but moves 1 tile per turn.' },
     { choice: 'Enhanced shatter', label: 'Enhanced shatter', description: 'Shatter also shatters target heros weapon. Their weapon will recover after 2 turns.' }
   ],
-  hp: 28,
-  speed: 12,
-  tileMove: 2,
+  hp: 55,
+  speed: 35,
+  tileMove: 4,
   passive: {
     label: 'Dig',
     description: 'Underground heroes will take half of their HP if miner uses pick axe on correct underground tile. Shatters underground heros passive.'
