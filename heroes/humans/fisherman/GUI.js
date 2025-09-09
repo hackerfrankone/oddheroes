cc23685ac89c34e1542c3ad32fdc08978c1358ac
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 11:45:40 -0400

    Update GUI.js

--- heroes/humans/fisherman/GUI.js
+++ heroes/humans/fisherman/GUI.js
@@ -4,19 +4,19 @@ const FishermanData = {
   category: 'Humans',
   id: 'humans',
   abilities: [
-    { choice: 'Kayak', label: 'Kayak', description: 'Requires no maintenance or gas. Moves 3 tiles on water. 80% chance to catch common fish. 50% to catch uncommon fish. 30% chance to catch a rare fish. Can carry 1 fish only.' },
+    { choice: 'Kayak', label: 'Kayak', description: 'Requires no maintenance or gas. Moves 3 tiles on water. 80% chance to catch common fish. 50% to catch uncommon fish. 20% chance to catch a rare fish. Can carry 1 fish only.' },
     { choice: 'Engine Boat', label: 'Fishing Boat', description: 'first turn & 2nd turns are gas and maintenance free, after that, every turn has 20% to need gas, 30% to need maintenance. Engine boat moves 8 tiles on water. Can carry an additional hero. 60% chance to catch common fish. 30% to catch uncommon fish. 10% chance to catch a rare fish.' }
   ],
   hp: 55,
   speed: 45,
   tileMove: 4,
   passive: {
-    label: 'End of every turn you fish. ',
+    label: 'End of every turn you fish. Common fish heals 10 HP, uncommon fish heals 30 HP, rare fish heals entire heros HP, worlds rarest fish heals entire heros HP and give +1 defense(cant stack, cant be debuffed)',
     description: ''
   },
   moveset: [
     {
-      label: 'Move 1: Fillet fish and store for later for or eat.',
+      label: 'Move 1: Fillet fish and store for later or eat.',
       description: 'A placeholder move for Fisherman in the battle arena.'
     },
     {
