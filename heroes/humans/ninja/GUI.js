a6f09a1c2af9d7ffee7687cdcefd9fda04274f07
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 18:28:01 -0400

    Update GUI.js

--- heroes/humans/ninja/GUI.js
+++ heroes/humans/ninja/GUI.js
@@ -5,7 +5,7 @@ const NinjaData = {
   id: 'humans',
   abilities: [
     { choice: 'Katana', label: 'Katana', description: 'This sword deals 25 HP.' },
-    { choice: 'Shurikens', label: 'Shuriken', description: 'Range is 5 tiles and deals 10 HP. You can choose to throw any amount at once. Dagger is your other weapon and deals 10 HP.' }
+    { choice: 'Shurikens', label: 'Shuriken', description: 'You carry 5 Shurikens, Range is 5 tiles and deals 10 HP. You can choose to throw any amount at once. Dagger is your other weapon and deals 10 HP.' }
   ],
   hp: 55,
   speed: 90,
@@ -20,7 +20,7 @@ const NinjaData = {
       description: 'A placeholder move for Ninja in the battle arena.'
     },
     {
-      label: 'Move 2: Grappling hook causes this hero to path to tree instantly. Can still move twice. If you target hero it deals 15 HP, you do not path to them.',
+      label: 'Move 2: Grappling hook causes this hero to path to tree instantly. Can still move twice. targeting enemy hero deals 15 HP, you do not path to them. Range 3 tiles.',
       description: 'Another placeholder move for Ninja in the battle arena.'
     }
   ]
