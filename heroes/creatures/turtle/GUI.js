5fa0038a4b865063ca93c572760431fbd6577a1b
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 10:23:32 -0400

    Update GUI.js

--- heroes/creatures/turtle/GUI.js
+++ heroes/creatures/turtle/GUI.js
@@ -1,10 +1,28 @@
-window.TurtleData = {
-  category: 'Creatures',
-  id: 'creatures',
+const TurtleData = {
   name: 'Turtle',
   icon: '🐢',
+  category: 'Creatures',
+  id: 'creatures',
   abilities: [
-    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+  ],
+  hp: 30,
+  speed: 10,
+  tileMove: 2,
+  passive: {
+    label: 'N/A',
+    description: ''
+  },
+  moveset: [
+    {
+      label: 'Move 1: Placeholder',
+      description: 'A placeholder move for Turtle in the battle arena.'
+    },
+    {
+      label: 'Move 2: Placeholder',
+      description: 'Another placeholder move for Turtle in the battle arena.'
+    }
   ]
 };
+window.TurtleData = TurtleData;
