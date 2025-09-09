414194077ab32df2e4d473a921ac1251042ca261
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 14:53:41 -0400

    Update GUI.js

--- heroes/uniques/ai/GUI.js
+++ heroes/uniques/ai/GUI.js
@@ -4,23 +4,23 @@ const AIData = {
   category: 'Uniques',
   id: 'uniques',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Unethical Hacking', label: 'Unethical Hacking', description: 'Hack into any device that contains wires, chips, hardwawre,software. Gain control of that target for 2 turns. This hero cannot move for 2 turns. No tile range. 1 time use per match.' },
+    { choice: 'Calculate Hero Data', label: 'Process data calculations', description: 'Decipher every enemy heros picked ability & item. ONLY when you have vision on enemy hero.' }
   ],
-  hp: 22,
+  hp: 11,
   speed: 20,
-  tileMove: 3,
+  tileMove: 2,
   passive: {
-    label: 'N/A',
-    description: ''
+    label: 'Central Processing Unit.',
+    description: 'Calculates Data...'
   },
   moveset: [
     {
-      label: 'Move 1: Placeholder',
+      label: 'Move 1: Robotic Punch deals 10 HP.',
       description: 'A placeholder move for AI in the battle arena.'
     },
     {
-      label: 'Move 2: Placeholder',
+      label: 'Move 2: Robotic Kick Deals 10 HP',
       description: 'Another placeholder move for AI in the battle arena.'
     }
   ]
