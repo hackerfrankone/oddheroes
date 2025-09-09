1769bb59db0c89f3d876635fccf982c986d711bf
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 14:43:38 -0400

    Update GUI.js

--- heroes/uniques/worm/GUI.js
+++ heroes/uniques/worm/GUI.js
@@ -4,23 +4,23 @@ const NightCrawlerData = {
   category: 'Uniques',
   id: 'uniques',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Enhanced Consume', label: 'Enhanced Consume', description: 'Consuming dead heroes grant an additional +10' },
+    { choice: 'Enhanced Burrowing', label: 'Enhanced Burrowing', description: 'Increase tile +3' }
   ],
-  hp: 18,
-  speed: 14,
-  tileMove: 2,
+  hp: 1,
+  speed: 1,
+  tileMove: 1,
   passive: {
-    label: 'N/A',
-    description: ''
+    label: 'Burrow, Consume.',
+    description: 'Dig underground leaving no broken tiles or trace. NightCrawler consumes any dead hero on ground and gaining +10 HP permanently. If friendly hero eats this hero it gains new max HP depending on NightCrawler HP.'
   },
   moveset: [
     {
-      label: 'Move 1: Placeholder',
+      label: 'Move 1: Burrow any direction.',
       description: 'A placeholder move for NightCrawler in the battle arena.'
     },
     {
-      label: 'Move 2: Placeholder',
+      label: 'Move 2: Unburrow to grant vision and granting food.',
       description: 'Another placeholder move for NightCrawler in the battle arena.'
     }
   ]
