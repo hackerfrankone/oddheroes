f448108a1610707a709eee162fd6d39f53b7df4f
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 11:31:06 -0400

    Update GUI.js

--- heroes/humans/fisherman/GUI.js
+++ heroes/humans/fisherman/GUI.js
@@ -4,23 +4,23 @@ const FishermanData = {
   category: 'Humans',
   id: 'humans',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Kayak', label: 'Kayak', description: 'Requires no maintenance or gas. Moves 3 tiles on water. 80% chance to catch common fish. 50% to catch uncommon fish. 30% chance to catch a rare fish. Can carry 1 fish only.' },
+    { choice: 'Engine Boat', label: 'Fishing Boat', description: 'first turn & 2nd turns are gas and maintenance free, after that, every turn has 20% to need gas, 30% to need maintenance. Engine boat moves 8 tiles on water. Can carry an additional hero. 60% chance to catch common fish. 30% to catch uncommon fish. 10% chance to catch a rare fish.' }
   ],
-  hp: 22,
-  speed: 18,
-  tileMove: 3,
+  hp: 55,
+  speed: 45,
+  tileMove: 4,
   passive: {
-    label: 'N/A',
+    label: 'End of every turn you fish. ',
     description: ''
   },
   moveset: [
     {
-      label: 'Move 1: Placeholder',
+      label: 'Move 1: Fillet fish and store for later for or eat.',
       description: 'A placeholder move for Fisherman in the battle arena.'
     },
     {
-      label: 'Move 2: Placeholder',
+      label: 'Move 2: Attack with your fishing pole and deal 15 HP.',
       description: 'Another placeholder move for Fisherman in the battle arena.'
     }
   ]
