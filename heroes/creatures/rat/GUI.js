c96c2846097658dec9182f2bcb6518aa728576b1
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 10:21:25 -0400

    Update GUI.js

--- heroes/creatures/rat/GUI.js
+++ heroes/creatures/rat/GUI.js
@@ -4,23 +4,23 @@ const RatData = {
   category: 'Creatures',
   id: 'creatures',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Scavenger', label: 'Scavenger', description: 'Use items on ground or find ways to enter chests or houses or any holes.' },
+    { choice: 'Escape', label: 'Escape', description: 'If this hero takes physical HP, this hero will gain +5 move tile on hero turn.' }
   ],
-  hp: 20,
+  hp: 15,
   speed: 25,
   tileMove: 3,
   passive: {
-    label: 'N/A',
-    description: ''
+    label: 'Detection',
+    description: 'This hero can detect anything underground. This hero will auto-eat enemy hero worm and gain new max heatlh 40 HP.'
   },
   moveset: [
     {
-      label: 'Move 1: Placeholder',
+      label: 'Move 1: Bite deals 10 HP.',
       description: 'A placeholder move for Rat in the battle arena.'
     },
     {
-      label: 'Move 2: Placeholder',
+      label: 'Move 2: Angry Bite deals 20 HP 2 turn CD.',
       description: 'Another placeholder move for Rat in the battle arena.'
     }
   ]
