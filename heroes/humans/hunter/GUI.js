d03cad14ba48dd954b2c40839229de65f7d2a137
hackerfrankone <hackerfrankone@gmail.com>
2025-09-09 14:18:16 -0400

    Update GUI.js

--- heroes/humans/hunter/GUI.js
+++ heroes/humans/hunter/GUI.js
@@ -4,8 +4,8 @@ const HunterData = {
   category: 'Humans',
   id: 'humans',
   abilities: [
-    { choice: 'Bow & Arrows', label: 'Bow & Arrows', description: 'This has 5 tile rage and deals 20 HP.' },
-    { choice: 'Crossbow & Bolts', label: 'Crossbow & Bolts', description: 'Shoot 5 bolts in one turn to deal 50 HP. Reloading bolts: 4 turn CD.' }
+    { choice: 'Bow Sniper', label: 'Bow & Arrows', description: 'This has 7 tile rage and deals 35 HP. 3 turn CD.' },
+    { choice: 'Crossbow & Bolts', label: 'Crossbow & Bolts', description: 'Shoot 5 bolts in one turn to deal 50 HP. 3 tile range. 3 turn CD. uses 5 bolts. Can spread the bolts however is appropiate.' }
   ],
   hp: 55,
   speed: 45,
@@ -16,11 +16,11 @@ const HunterData = {
   },
   moveset: [
     {
-      label: 'Move 1: Shoot Bow or CrossBow HP depends on ability picked. You can shoot 5 different targets with crossbow at once within tiles and each bolt deals 10HP.',
+      label: 'Move 1: Shoot Bow or CrossBow. Bow deals 20 HP with 4 tile range. Cross Bow deals 30 HP with 3 tile range.',
       description: 'A placeholder move for Hunter in the battle arena.'
     },
     {
-      label: 'Move 2: Craft home made Arrows or Bolts. Craft 5 arrows at once or 15 bolts at once. These are half HP from original arrows and bolts. Cannot use this move untill current quiver is empty.',
+      label: 'Move 2: Craft Arrows or Bolts fill up Quiver.',
       description: 'Another placeholder move for Hunter in the battle arena.'
     }
   ]
