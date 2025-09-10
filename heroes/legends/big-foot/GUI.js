e2bb47df35d488897b0ed6e7cbe6ff7b3186b241
hackerfrankone <hackerfrankone@gmail.com>
2025-09-10 15:40:54 -0400

    Update GUI.js

--- heroes/legends/big-foot/GUI.js
+++ heroes/legends/big-foot/GUI.js
@@ -1,10 +1,28 @@
-window.BigFootData = {
+const BigFootData = {
+  name: 'BigFoot',
+  icon: '🦶🏽',
   category: 'Legends',
   id: 'legends',
-  name: 'BigFoot',
-  icon: '🦶',
   abilities: [
-    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+  ],
+  hp: 40,
+  speed: 12,
+  tileMove: 2,
+  passive: {
+    label: 'N/A',
+    description: ''
+  },
+  moveset: [
+    {
+      label: 'Move 1: Placeholder',
+      description: 'A placeholder move for BigFoot in the battle arena.'
+    },
+    {
+      label: 'Move 2: Placeholder',
+      description: 'Another placeholder move for BigFoot in the battle arena.'
+    }
   ]
 };
+window.BigFootData = BigFootData;
