3b66c8c3fd9aa00305d63b540a274d21de50a780
hackerfrankone <hackerfrankone@gmail.com>
2025-09-11 00:36:35 -0400

    Update GUI.js

--- heroes/legends/merfolk/GUI.js
+++ heroes/legends/merfolk/GUI.js
@@ -1,10 +1,28 @@
-window.MerfolkData = {
-  category: 'legends',
-  id: 'legends',
+const MerfolkData = {
   name: 'Merfolk',
-  icon: '🧜‍♂️',
+  icon: '🧜',
+  category: 'Legends',
+  id: 'legends',
   abilities: [
-    { choice: 'ability1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'ability2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+  ],
+  hp: 100,
+  speed: 55,
+  tileMove: 10,
+  passive: {
+    label: '+25 HP and passively regenerates 5 HP end of every turn.',
+    description: ''
+  },
+  moveset: [
+    {
+      label: 'Move 1: Placeholder',
+      description: 'A placeholder move for Merfolk in the battle arena.'
+    },
+    {
+      label: 'Move 2: Placeholder',
+      description: 'Another placeholder move for Merfolk in the battle arena.'
+    }
   ]
 };
+window.MerfolkData = MerfolkData;
