b75e95a6bbda25f3ad3a708ca0cbab2d5efa486c
hackerfrankone <hackerfrankone@gmail.com>
2025-09-11 10:17:18 -0400

    Update GUI.js

--- heroes/legends/dragon/GUI.js
+++ heroes/legends/dragon/GUI.js
@@ -4,23 +4,23 @@ const DragonData = {
   category: 'Legends',
   id: 'legends',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Fly', label: 'Fly', description: 'Fly above the sky. Cannot take physical HP. Recover 40 HP. 2 turn CD.' },
+    { choice: 'Enhance Fiery Aura', label: 'Enhance Fiery Aura', description: 'Expands the area of the fiery aura to 2 tiles from hero.' }
   ],
-  hp: 60,
-  speed: 15,
-  tileMove: 3,
+  hp: 100,
+  speed: 75,
+  tileMove: 8,
   passive: {
-    label: 'N/A',
-    description: ''
+    label: 'Fiery Aura',
+    description: 'This hero contains a fiery aura. Nearby tiles within this hero takes 10 HP'
   },
   moveset: [
     {
-      label: 'Move 1: Placeholder',
+      label: 'Move 1: Fire Breath deals 40 HP, 4 tile range. 2 turn CD.',
       description: 'A placeholder move for Dragon in the battle arena.'
     },
     {
-      label: 'Move 2: Placeholder',
+      label: 'Move 2: Charge & Claw deals 20 HP. Melee range.',
       description: 'Another placeholder move for Dragon in the battle arena.'
     }
   ]
