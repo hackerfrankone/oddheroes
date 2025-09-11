13be347bf0b880ed7341737505cc1c9c47ef9150
hackerfrankone <hackerfrankone@gmail.com>
2025-09-11 11:06:01 -0400

    Update GUI.js

--- heroes/legends/dinosaur/GUI.js
+++ heroes/legends/dinosaur/GUI.js
@@ -4,29 +4,25 @@ const DinosaurData = {
   category: 'Legends',
   id: 'legends',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-3', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-4', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-5', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-6', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Tyrannosaurus', label: 'Tyrannosaurus', description: 'Bite deals additional +10 HP.' },
+    { choice: 'Velociraptor', label: 'Velociraptor', description: 'Reduced HP to 100. speed 99, tile move to 9.' },
+    { choice: 'Spinosaurus', label: 'Spinosaurus', description: 'Reduced HP to 120. This hero is land & water. In water grants +4 and +20 to Bite.' },
+    { choice: 'Triceratops', label: 'Triceratops', description: 'Increased health to 200 HP. Reduce speed 15, tile move 3.' },
+    { choice: 'pterodactyl', label: 'pterodactyl', description: 'Reduced health to 85, speed 80, tile move to 10.' },
+    { choice: 'Mosasaurus', label: 'Mosasaurus', description: 'Only water, Bite deals additional +10 HP. ' }
   ],
-  hp: 50,
-  speed: 10,
-  tileMove: 2,
+  hp: 150,
+  speed: 45,
+  tileMove: 5,
   passive: {
-    label: 'N/A',
-    description: ''
+    label: 'Execute',
+    description: 'Landing the killing blow on human or animal grants additional +10 HP, +10 to physical HP, +2 tile, +10 speed, heals 30 HP.' 
   },
   moveset: [
     {
-      label: 'Move 1: Placeholder',
+      label: 'Move 1: Bite deals 40 HP.',
       description: 'A placeholder move for Dinosaur in the battle arena.'
     },
-    {
-      label: 'Move 2: Placeholder',
-      description: 'Another placeholder move for Dinosaur in the battle arena.'
-    }
   ]
 };
 window.DinosaurData = DinosaurData;
