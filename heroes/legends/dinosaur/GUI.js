4b1a12731abfe19bb42fea4be1b63c7d02693da0
hackerfrankone <hackerfrankone@gmail.com>
2025-09-11 09:22:43 -0400

    Update GUI.js

--- heroes/legends/dinosaur/GUI.js
+++ heroes/legends/dinosaur/GUI.js
@@ -1,13 +1,32 @@
-window.DinosaurData = {
+const DinosaurData = {
+  name: 'Dinosaur',
+  icon: '🦖',
   category: 'Legends',
   id: 'legends',
-  name: 'Dinosaur',
-  icon: '🥚',
   abilities: [
-    { choice: 'ability1', label: 'Tyrannosaurus', description: 'place-holder' },
-    { choice: 'ability2', label: 'Spinosaurus', description: 'place-holder' },
-    { choice: 'ability3', label: 'Pterodactyl', description: 'place-holder' },
-    { choice: 'ability4', label: 'Velociraptor', description: 'place-holder' },
-    { choice: 'ability5', label: 'Dilophosaurus', description: 'place-holder' }
+    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
+    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' },
+    { choice: 'place-holder-3', label: 'place-holder', description: 'place-holder' },
+    { choice: 'place-holder-4', label: 'place-holder', description: 'place-holder' },
+    { choice: 'place-holder-5', label: 'place-holder', description: 'place-holder' },
+    { choice: 'place-holder-6', label: 'place-holder', description: 'place-holder' }
+  ],
+  hp: 50,
+  speed: 10,
+  tileMove: 2,
+  passive: {
+    label: 'N/A',
+    description: ''
+  },
+  moveset: [
+    {
+      label: 'Move 1: Placeholder',
+      description: 'A placeholder move for Dinosaur in the battle arena.'
+    },
+    {
+      label: 'Move 2: Placeholder',
+      description: 'Another placeholder move for Dinosaur in the battle arena.'
+    }
   ]
 };
+window.DinosaurData = DinosaurData;
