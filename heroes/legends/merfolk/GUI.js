24028efd1232ca8633fd7cdab6440d0e9bd40e59
hackerfrankone <hackerfrankone@gmail.com>
2025-09-11 08:11:42 -0400

    Update GUI.js

--- heroes/legends/merfolk/GUI.js
+++ heroes/legends/merfolk/GUI.js
@@ -4,23 +4,23 @@ const MerfolkData = {
   category: 'Legends',
   id: 'legends',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Enhanced Vitality', label: 'Vitality', description: '+25 HP.' },
+    { choice: 'Enhanced Tsunami', label: 'place-holder', description: 'Tsunami deals 5 more HP +2 tile range with a new tile structure that is 3x7.' }
   ],
   hp: 100,
   speed: 55,
   tileMove: 10,
   passive: {
-    label: 'Born Vitality',
+    label: 'Vitality',
     description: '+25 HP and passively regenerates 5 HP end of every turn'
   },
   moveset: [
     {
-      label: 'Move 1: Tsunami deals 25 HP. From this hero in a straight line 5 tiles out.',
+      label: 'Move 1: Tsunami deals 25 HP. From this hero in a straight line 5 tiles out. 2x5 1 turn CD.',
       description: 'A placeholder move for Merfolk in the battle arena.'
     },
     {
-      label: 'Move 2: Trcickster uses last move from enemy hero that is within 5 tiles of this hero. Target new Targets',
+      label: 'Move 2: Trident Swipe deals 30 HP. melee range.',
       description: 'Another placeholder move for Merfolk in the battle arena.'
     }
   ]
