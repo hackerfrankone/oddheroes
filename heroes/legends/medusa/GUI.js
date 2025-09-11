9b1cacc07d30ce4e9e525b95abf31b3a85ec3f20
hackerfrankone <hackerfrankone@gmail.com>
2025-09-11 10:06:47 -0400

    Update GUI.js

--- heroes/legends/medusa/GUI.js
+++ heroes/legends/medusa/GUI.js
@@ -4,23 +4,23 @@ const MedusaData = {
   category: 'Legends',
   id: 'legends',
   abilities: [
-    { choice: 'place-holder-1', label: 'place-holder', description: 'place-holder' },
-    { choice: 'place-holder-2', label: 'place-holder', description: 'place-holder' }
+    { choice: 'Bow & Arrow', label: 'Bow & Arrow', description: 'Carry 10 arrows. Each arrow deals 30 HP. tile range 7.' },
+    { choice: 'Sword & Shield', label: 'Sword & Shield', description: 'Sword deals 25 HP. Shield has a 20% Chance to dodge melee & ranged attacks.' }
   ],
-  hp: 30,
-  speed: 16,
-  tileMove: 3,
+  hp: 85,
+  speed: 45,
+  tileMove: 5,
   passive: {
-    label: 'N/A',
-    description: ''
+    label: 'Gaze & Daze',
+    description: 'heroes that are 1 tile close to this hero are stunned for 2 turns. Same target cannot be Gaze & Daze untill 4 turns. Gaze & Daze can stun up to 2 heroes at the same time.'
   },
   moveset: [
     {
-      label: 'Move 1: Placeholder',
+      label: 'Move 1: Rattler Strike deals 20 HP. Next turn, when this hero attacks the same target, it will deal triple HP. 2 tile range.',
       description: 'A placeholder move for Medusa in the battle arena.'
     },
     {
-      label: 'Move 2: Placeholder',
+      label: 'Move 2: Slither quickly. Move additional 5 tiles and camofluage regardless of any structure.',
       description: 'Another placeholder move for Medusa in the battle arena.'
     }
   ]
